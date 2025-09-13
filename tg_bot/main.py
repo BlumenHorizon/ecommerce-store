fecc7318d8414c779f8bd3361e177b6ea5cb5458
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-09-13 17:04:16 +0200

    refactor(tg): 'send_message_to_telegram' accepts list of chat ids now instead of fetching

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -9,7 +9,7 @@ TELEGRAM_BOT_TOKEN = settings.TELEGRAM_BOT_TOKEN
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 
-async def send_message_to_telegram_async(chat_id, text):
+async def send_message_to_telegram_async(chat_id: str, text: str):
     try:
         await bot.send_message(chat_id, text, parse_mode="Markdown")
     except Exception as e:
@@ -19,20 +19,18 @@ async def send_message_to_telegram_async(chat_id, text):
         )
 
 
-async def send_messages_to_multiple_chat_ids(chat_ids, text):
+async def send_messages_to_multiple_chat_ids(chat_ids: list[str], text: str):
     tasks = [send_message_to_telegram_async(chat_id, text) for chat_id in chat_ids]
     await asyncio.gather(*tasks)
 
 
-def send_message_to_telegram(text: str):
+def send_message_to_telegram(text: str, telegram_chat_ids: list[str]):
     """
     Sends a message to multiple chat_ids using asyncio.
     :param text: The message text to send.
     :param chat_ids: A list of chat IDs to send the message to.
     """
     logger = logging.getLogger("telegramBot")
-    telegram_chat_ids = os.getenv("TELEGRAM_CHAT_IDS", "")
-    chat_ids_list = telegram_chat_ids.split(",") if telegram_chat_ids else []
     try:
         try:
             loop = asyncio.get_event_loop()
@@ -42,11 +40,11 @@ def send_message_to_telegram(text: str):
 
         if loop.is_running():
             asyncio.ensure_future(
-                send_messages_to_multiple_chat_ids(chat_ids_list, text)
+                send_messages_to_multiple_chat_ids(telegram_chat_ids, text)
             )
         else:
             loop.run_until_complete(
-                send_messages_to_multiple_chat_ids(chat_ids_list, text)
+                send_messages_to_multiple_chat_ids(telegram_chat_ids, text)
             )
     except Exception as e:
         logger.error(f"Error in send_message_to_telegram: {e}")
