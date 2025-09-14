efb7896f9fecbc1e5bd43c2aea68115c0280476b
Leviinson <votosi197@gmail.com>
2025-09-14 15:37:05 +0300

    feat(tg): 'send_document_to_telegram' created

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -3,9 +3,64 @@ import logging
 
 from django.conf import settings
 from telegram import Bot
+from telegram.request import HTTPXRequest
 
 TELEGRAM_BOT_TOKEN = settings.TELEGRAM_BOT_TOKEN
-bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+request = HTTPXRequest(
+    connection_pool_size=1,
+    read_timeout=900,
+    write_timeout=900,
+    connect_timeout=60,
+    pool_timeout=60,
+)
+
+bot = Bot(token=TELEGRAM_BOT_TOKEN, request=request)
+
+
+async def send_document_to_telegram_async(
+    chat_id: str, file_path: str, caption: str = None
+):
+    with open(file_path, "rb") as f:
+        await bot.send_document(
+            chat_id=chat_id,
+            document=f,
+            caption=caption,
+            parse_mode="Markdown",
+            read_timeout=900,
+            write_timeout=900,
+            connect_timeout=60,
+            pool_timeout=60,
+        )
+
+
+async def send_documents_to_multiple_chat_ids(
+    chat_ids: list[str], file_path: str, caption: str = None
+):
+    tasks = [
+        send_document_to_telegram_async(chat_id, file_path, caption)
+        for chat_id in chat_ids
+    ]
+    await asyncio.gather(*tasks)
+
+
+def send_document_to_telegram(
+    file_path: str, telegram_chat_ids: list[str], caption: str = None
+):
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    if loop.is_running():
+        asyncio.ensure_future(
+            send_documents_to_multiple_chat_ids(telegram_chat_ids, file_path, caption)
+        )
+    else:
+        loop.run_until_complete(
+            send_documents_to_multiple_chat_ids(telegram_chat_ids, file_path, caption)
+        )
 
 
 async def send_message_to_telegram_async(chat_id: str, text: str):
