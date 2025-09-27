ff563c714fcfa89a2184ec5e69f90c28b2822d72
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-09-27 13:37:14 +0200

    chore(settings): settings for development added

---
+++ core/cities/settings/dev.py
@@ -0,0 +1,10 @@
+from core.cities.settings.base import *
+
+TIME_ZONE = "Europe/Berlin"
+LANGUAGES = [
+    ("ru", "🇷🇺 Русский"),
+    ("de", "🇩🇪 Deutsch"),
+    ("en", "🇺🇸 English"),
+    ("uk", "🇺🇦 Українська"),
+]
+LANGUAGE_CODE = "ru"
